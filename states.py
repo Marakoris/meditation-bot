a247ea0b2767de4dfaa4cbf5148de91132f383c9
Nina <96581858+Marakoris@users.noreply.github.com>
2025-07-07 14:32:00 +0300

    Create states.py

---
+++ states.py
@@ -0,0 +1,15 @@
+# states.py
+from aiogram.fsm.state import State, StatesGroup
+
+class MeditationStates(StatesGroup):
+    waiting_for_comment = State()
+    waiting_for_rating = State()
+    waiting_for_marathon_title = State()
+    waiting_for_marathon_description = State()
+    waiting_for_marathon_dates = State()
+    waiting_for_marathon_goal = State()
+
+class DialogueStates(StatesGroup):
+    waiting_for_manual_entry = State()
+    confirming_manual_entry = State()
+    in_dialogue = State()
